6b49374caabcfbc8d034ed6283c6e7465ceda517
火烈鸟 <250023777@qq.com>
2024-07-11 21:47:38 +0800

    mek

---
+++ core/src/test/java/com/alibaba/fastjson2/issues_2700/Issue2771.java
@@ -0,0 +1,15 @@
+package com.alibaba.fastjson2.issues_2700;
+
+/**
+ * @author: mek
+ * Date: 2024/7/11
+ * Time: 21:43
+ * vx: 250023777
+ * Description: 2771
+ * @version: 1.0
+ */
+public class Issue2771 {
+    public static void main(String[] args) {
+
+    }
+}
